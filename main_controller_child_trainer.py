8b3cf39df2e705b3648d77516a02e1fc7e721258
MingukKANG <30516904+MINGUKKANG@users.noreply.github.com>
2018-05-24 22:20:32 +0900

    Update main_controller_child_trainer.py

--- main_controller_child_trainer.py
+++ main_controller_child_trainer.py
@@ -28,7 +28,7 @@ DEFINE_string("val_data_dir", "./data/valid", "")
 DEFINE_string("test_data_dir", "./data/test", "")
 DEFINE_integer("channel",1, "MNIST: 1, Cifar10: 3")
 DEFINE_integer("img_size", 32, "if size_img = 32 -> image: 32 x 32 x channel")
-DEFINE_integer("n_aug_img",1 , "num_img: 50000 -> aug_img: 200000")
+DEFINE_integer("n_aug_img",2 , "if 2: num_img: 55000 -> aug_img: 110000, elif 1: False")
 ##########################################################################
 
 DEFINE_boolean("reset_output_dir", True, "Delete output_dir if exists.")
